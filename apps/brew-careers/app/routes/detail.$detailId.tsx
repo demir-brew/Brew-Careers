1ef3966667f739f70b389fdfd9992f58eaa1ca1d
batuhangoren99@hotmail.com <batuhan@brewww.com>
2023-11-17 14:13:34 +0300

    feat(BC-3): routing issues is done

--- apps/brew-careers/app/routes/detail.$detailId.tsx
+++ apps/brew-careers/app/routes/detail.$detailId.tsx
@@ -1,31 +1,29 @@
-import { Outlet, useParams } from "@remix-run/react";
+import { Outlet, useLoaderData, useParams } from "@remix-run/react";
 
-import type { LoaderFunction } from "@remix-run/node";
+import { type LoaderFunction } from "@remix-run/node";
 
 export let loader: LoaderFunction = async ({ params }) => {
   try {
     const { detailId } = params;
-    console.log("id", detailId);
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/todos/${detailId}`
     );
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
+    console.log("response", response);
     const data = await response.json();
+
     return data;
   } catch (error) {
-    console.error("Loader error:", error);
     throw new Error("Failed to load data");
   }
 };
-export default function Detail({ data }) {
+export default function Detail() {
+  const data = useLoaderData<typeof loader>();
   let { detailId } = useParams();
   return (
     <div>
       Detail {detailId}
-      {data}
-      <Outlet />
+      {data.title}
+      {/* <Outlet /> */}
     </div>
   );
 }
