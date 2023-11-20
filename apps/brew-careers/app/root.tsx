f5d92536c8311a43addaeb25c791d13c15756e8d
salihcandusmez <salih@brewww.com>
2023-11-20 15:44:01 +0300

    feat(BC-7-frontend-development-for-adding-global-css-and-creating-home-page): create public files and homepage

--- apps/brew-careers/app/root.tsx
+++ apps/brew-careers/app/root.tsx
@@ -1,31 +1,67 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { LiveReload, Outlet, ScrollRestoration } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta content="IE=Edge,chrome=1" http-equiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <title>Careers - Jobs - Brew Interactive</title>
+        <meta content="Careers - Jobs - Brew Interactive" name="description" />
+        <meta
+          content="Careers - Jobs - Brew Interactive"
+          itemProp="description"
+        />
+        <meta content="summary_large_image" name="twitter:card" />
+        <meta content="Careers - Jobs - Brew Interactive" property="og:title" />
+        <meta
+          content="Careers - Jobs - Brew Interactive"
+          property="og:description"
+        />
+        <meta content="images/share_image.png" property="og:image" />
+        <meta content="1200" property="og:image:width" />
+        <meta content="630" property="og:image:height" />
+        <meta content="https://brew.recruitee.com/" property="og:url" />
+        <meta content="Brew Interactive" property="og:site_name" />
+        <meta content="website" property="og:type" />
+        <link href="https://brew.recruitee.com/" rel="canonical" />
+        <meta name="csrf-param" content="authenticity_token" />
+        <meta
+          name="csrf-token"
+          content="OC2keB7HgSd3CLO3oh+pE5wPm6KXcOGD6x/R4ey0QRqKXEAjBCSY9lWsKcGzR7Bq15gzmXBK4EgyfEfceDpWag=="
+        />
+
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="favicon-4c9bc445d00feebd8833a9dc049e02742732854c8e58a199d3b2c3f9037d42c8.ico"
+        />
+        <link
+          rel="stylesheet"
+          media="screen"
+          href="css/careers-e3bd8e42fc609055a4f737777f8c5d912a700894765301a04d71fabebf7dd5fa.css"
+        />
+        <link
+          rel="stylesheet"
+          media="screen"
+          href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
+          data-company-font="true"
+        />
+        <link
+          rel="stylesheet"
+          media="screen"
+          href="css/style_1652974541.css"
+          data-company-css="true"
+        />
+        <script src="js/rt_app-b25d8d9fbffd0631b15547f64ee76931f266926fea014685d7118fe209ad476e.js"></script>
+        <script src="js/careers-ccdcff3ffcc048d61156e89009d5b48a8f4fd7b63e71740419c7d07494229d4f.js"></script>
+
+        <script src="js/errors_handler-18c907b27701ecba3737f74ec2d352eff7ccdedb59a3f25752ed3f84d9801c7c.js"></script>
       </head>
       <body>
         <Outlet />
         <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
--- apps/brew-careers/app/routes/_index.tsx
+++ apps/brew-careers/app/routes/_index.tsx
@@ -2,13 +2,434 @@ import { Link } from "@remix-run/react";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <Link to="/detail/1"> tıkla</Link>
+    <body className="offers-controller index" id="index">
+      <div id="components-container">
+        <div className="header-component component" id="section-112287">
+          <div
+            className="header-cover-image white-left-logo"
+            style={{
+              backgroundImage: "url(images/normal_photo_1551198210.jpg)",
+            }}
+          >
+            <div
+              className="overlay"
+              style={{ backgroundColor: "#222933", opacity: 0 }}
+            ></div>
+          </div>
+
+          <div className="white-left-logo">
+            <nav className="navbar navbar-default" role="navigation">
+              <div className="container">
+                <h1 className="brand">
+                  <a href="/">
+                    <img
+                      title="Careers - Jobs - Brew Interactive"
+                      alt="Careers - Jobs - Brew Interactive"
+                      src="images/thumb_photo_1652974157.png"
+                    />
+                  </a>
+                </h1>
+                <div className="pull-right buttons-wrap">
+                  <div className="hidden-xs hidden-sm buttons-wrap">
+                    <a
+                      className="company-link btn btn-primary hidden-xs"
+                      target="_blank"
+                      rel="nofollow noopener"
+                      href="https://brewww.com"
+                    >
+                      Company website
+                      <i className="fa fa-arrow-circle-right"></i>
+                    </a>
+
+                    <ul className="nav navbar-nav socials hidden-xs navbar-right">
+                      <li>
+                        <a
+                          target="_blank"
+                          rel="noopener"
+                          href="https://www.linkedin.com/company/brew-interactive/"
+                        >
+                          <i className="fa fa-linkedin-square"></i>
+                        </a>
+                      </li>
+
+                      <li>
+                        <a
+                          target="_blank"
+                          rel="noopener"
+                          href="http://twitter.com/brewinteractive"
+                        >
+                          <i className="fa fa-twitter-square"></i>
+                        </a>
+                      </li>
+
+                      <li>
+                        <a
+                          target="_blank"
+                          rel="noopener"
+                          href="https://instagram.com/brew_interactive"
+                        >
+                          <i className="fa fa-instagram"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <button
+                  className="navbar-toggle"
+                  data-target="#navbar-collapse"
+                  data-toggle="collapse"
+                  type="button"
+                >
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbar-collapse">
+                  <ul className="menu nav navbar-nav">
+                    <li>
+                      <a href="/">Home</a>
+                    </li>
+
+                    <li>
+                      <a href="/#section-112289">Jobs</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+            <div className="content container">
+              <div className="info-container">
+                <div className="info">
+                  <h2>Welcome to the Brew Careers</h2>
+                  <h3>We are looking for talents to blend</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="spacing"></div>
+        </div>
+
+        <div className="text-component component" id="section-112288">
+          <div className="no-photo container">
+            <h3 className="section-title">We are Brew Interactive</h3>
+
+            <h4 className="section-subtitle">
+              Named before 3rd wave coffee shops and home-brewing became popular
+            </h4>
+
+            <div className="text row">
+              <div className="body col-xs-12">
+                <p className="text-center">
+                  <span style={{ fontSize: "16px" }}>
+                    We are a UX and frontend focused development studio, serving
+                    domestic and international clients with their software
+                    products. Here at Brew, we love good UX and design; we love
+                    good engineering and development practices; we love
+                    open-source; we love agile; we love code-reviews and
+                    mentoring; we love working with alpha releases…
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="offers-component component" id="section-112289">
+          <div className="container">
+            <h3 className="section-title">Join the team</h3>
+
+            <h4 className="section-subtitle">Current openings</h4>
+
+            <div className="simple-view">
+              <div className="jobs" id="jobs-list">
+                <div className="filters-container"></div>
+                <div className="clearfix"></div>
+                <div className="list-container list">
+                  <div className="job" id="job-1104965">
+                    <div className="row">
+                      <div className="col-md-8 col-xs-12 col-centered">
+                        <h5 className="job-title">
+                          <a href="/o/business-analyst">Business Analyst</a>
+                        </h5>
+
+                        <div className="department">Product</div>
+
+                        <ul className="meta">
+                          <li className="job-location">Remote job</li>
+                        </ul>
+                      </div>
+                      <div className="col-md-4 col-xs-12 apply-now col-centered">
+                        <a
+                          className="btn btn-primary"
+                          href="/o/business-analyst"
+                        >
+                          View job
+                        </a>
+                      </div>
+
+                      <div className="job-data hidden">
+                        <div className="language">["en"]</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="job" id="job-432284">
+                    <div className="row">
+                      <div className="col-md-8 col-xs-12 col-centered">
+                        <h5 className="job-title">
+                          <a href="/o/internship-at-brew">Internship at Brew</a>
+                        </h5>
+
+                        <div className="department">Development</div>
+
+                        <ul className="meta">
+                          <li className="job-location">Remote job</li>
+                        </ul>
+                      </div>
+                      <div className="col-md-4 col-xs-12 apply-now col-centered">
+                        <a
+                          className="btn btn-primary"
+                          href="/o/internship-at-brew"
+                        >
+                          View job
+                        </a>
+                      </div>
+
+                      <div className="job-data hidden">
+                        <div className="language">["en"]</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="job" id="job-585173">
+                    <div className="row">
+                      <div className="col-md-8 col-xs-12 col-centered">
+                        <h5 className="job-title">
+                          <a href="/o/uxui-designer-remote-or-inoffice">
+                            UX/UI Designer (remote or in-office)
+                          </a>
+                        </h5>
+
+                        <div className="department">Product</div>
+
+                        <ul className="meta">
+                          <li className="job-location">Remote job</li>
+                        </ul>
+                      </div>
+                      <div className="col-md-4 col-xs-12 apply-now col-centered">
+                        <a
+                          className="btn btn-primary"
+                          href="/o/uxui-designer-remote-or-inoffice"
+                        >
+                          View job
+                        </a>
+                      </div>
+
+                      <div className="job-data hidden">
+                        <div className="language">["en"]</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="job" id="job-707549">
+                    <div className="row">
+                      <div className="col-md-8 col-xs-12 col-centered">
+                        <h5 className="job-title">
+                          <a href="/o/backend-engineer-node-net-core-remote-or-inoffice">
+                            Backend Engineer (Node, .Net Core) (remote or
+                            in-office)
+                          </a>
+                        </h5>
+
+                        <div className="department">Development</div>
+
+                        <ul className="meta">
+                          <li className="job-location">Remote job</li>
+                        </ul>
+                      </div>
+                      <div className="col-md-4 col-xs-12 apply-now col-centered">
+                        <a
+                          className="btn btn-primary"
+                          href="/o/backend-engineer-node-net-core-remote-or-inoffice"
+                        >
+                          View job
+                        </a>
+                      </div>
+
+                      <div className="job-data hidden">
+                        <div className="language">["en"]</div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="job" id="job-264299">
+                    <div className="row">
+                      <div className="col-md-8 col-xs-12 col-centered">
+                        <h5 className="job-title">
+                          <a href="/o/frontend-engineer-remote-or-inoffice">
+                            Frontend Engineer (remote or in-office)
+                          </a>
+                        </h5>
+
+                        <div className="department">Development</div>
+
+                        <ul className="meta">
+                          <li className="job-location">Remote job</li>
+                        </ul>
+                      </div>
+                      <div className="col-md-4 col-xs-12 apply-now col-centered">
+                        <a
+                          className="btn btn-primary"
+                          href="/o/frontend-engineer-remote-or-inoffice"
+                        >
+                          View job
+                        </a>
+                      </div>
+
+                      <div className="job-data hidden">
+                        <div className="language">["en"]</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="empty-state hidden">
+                  <h3>No job openings found</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="perks-component component" id="section-112322">
+          <div
+            className="perks-background-image"
+            style={{
+              backgroundImage: "url(images/normal_photo_1551257038.jpg)",
+              backgroundColor: "white",
+            }}
+          >
+            <div
+              className="overlay"
+              style={{ backgroundColor: "#222933", opacity: 0 }}
+            ></div>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <h3 className="section-title">Perks &amp; Benefits</h3>
+
+                  <h4 className="section-subtitle">
+                    A selection of what we have to offer
+                  </h4>
+                </div>
+              </div>
+              <div className="entry row row-centered">
+                <div className="col-md-4">
+                  <ul>
+                    <li>
+                      Ability to work remote or in our offices in Urla and Izmir
+                      downtown
+                    </li>
+
+                    <li>Employee oriented, flexible working environment</li>
+
+                    <li>No extra hours</li>
+                  </ul>
+                </div>
+
+                <div className="col-md-4">
+                  <ul>
+                    <li>
+                      Fast-paced learning environment in a multi-talent team
+                    </li>
+
+                    <li>
+                      Private health insurance, meal vouchers, co-working
+                      memberships
+                    </li>
+
+                    <li>Competitive salary</li>
+                  </ul>
+                </div>
+
+                <div className="col-md-4">
+                  <ul>
+                    <li>Professional support for career development</li>
+
+                    <li>Top of the line computing equipment and software</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="html-component component" id="section-325655">
+          <div className="container">
+            <div className="text row">
+              <div className="body col-xs-12">
+                <div className="container">
+                  <h3 className="section-title">
+                    Our team thinks that BREW is GREAT!
+                  </h3>
+
+                  <h4 className="section-subtitle">
+                    This is where you thrive!
+                  </h4>
+
+                  <div className="text row">
+                    <div className="photo col-md-2">
+                      <img
+                        className="img-rounded"
+                        src="images/BREW_INTERACTIVE__2023_Certification_Badge.png"
+                      />
+                    </div>
+                    <div className="body col-md-10">
+                      <p>
+                        BREW has officially been recognized by the Great Place
+                        to Work Institute based on feedback from our amazing
+                        team. The Institute is committed to improving workplaces
+                        so that every employee can thrive, and their survey
+                        provides an objective assessment of the employee
+                        experience.
+                      </p>
+                      <p>
+                        Thanks to our team's positive responses, we've earned
+                        the Great Place to Work certification with a 95% score.
+                        At BREW, "Being a team" has always been a top value, and
+                        we're grateful to our team for making it a reality
+                        through their unwavering support and presence.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <Link to="/detail/2"> tıkla</Link>
-      </div>
-    </>
+
+      <footer className="container">
+        <a
+          target="_blank"
+          className="footer-logo"
+          rel="noopener"
+          href="https://recruitee.com"
+        >
+          <span>
+            <span
+              className="translation_missing"
+              title="translation missing: en.careers.common.hiring_with"
+            >
+              Hiring With
+            </span>
+          </span>
+          <img src="images/logo__horizontal-95300540fb91da99f96dab9a76748ac00a0b39a5f02d432d373259194112dabb.svg" />
+        </a>
+      </footer>
+    </body>
   );
 }
